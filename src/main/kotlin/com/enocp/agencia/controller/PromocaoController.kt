c8b3f467f3fbbfd97ab0683e70c43d3037e3e134
pierreEnoc <pierre_enoc@yahoo.fr>
2020-05-06 15:02:27 -0300

    parametters

--- src/main/kotlin/com/enocp/agencia/controller/PromocaoController.kt
+++ src/main/kotlin/com/enocp/agencia/controller/PromocaoController.kt
@@ -2,6 +2,7 @@ package com.enocp.agencia.controller
 
 import com.enocp.agencia.model.Promocao
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
@@ -16,6 +17,6 @@ class PromocaoController {
     fun bunjourLemonde(): String {
         return "Bonjour le monde!"
     }
-    @RequestMapping(value = ["/promocoes"], method = arrayOf(RequestMethod.GET))
-    fun getPromocao() = promocoes[3]
+    @RequestMapping(value = ["/promocoes/{id}"], method = arrayOf(RequestMethod.GET))
+    fun getPromocao(@PathVariable id:Long) = promocoes[id]
 }
